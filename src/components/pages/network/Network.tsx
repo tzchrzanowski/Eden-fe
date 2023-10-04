05a0cd934577f3ccb3c492b7b601530f9d06dc85
tzchrzanowski <tzchrzanowski@gmail.com>
2023-10-04 10:55:15 +0800

    added icons and some network structure

--- src/components/pages/network/Network.tsx
+++ src/components/pages/network/Network.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import './Network.css';
 import TopNavigation from "components/top-navigation/TopNavigation";
-
+import BinaryTree, {TreeNodeInterface } from "./binary-tree/BinaryTree";
 export function Network() {
+
+    const rootNode: TreeNodeInterface = {
+        name: 'Root',
+        caption: 'The Root Node',
+        leftNode: {
+            name: 'Left Node',
+            caption: 'A Left Child Node',
+            leftNode: null,
+            rightNode: null,
+        },
+        rightNode: {
+            name: 'Right Node',
+            caption: 'A Right Child Node',
+            leftNode: null,
+            rightNode: null,
+        },
+    };
+
     return (
         <div className={"pageWrapper"}>
             <TopNavigation />
             <div className={"pageContent"}>
                 <p>Network Page</p>
-                <p>Binary tree will go here...</p>
+                <BinaryTree rootNode={rootNode} />
             </div>
         </div>
     )
---
+++ src/components/pages/network/binary-tree/BinaryTree.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import './BinaryTree.css';
+
+export interface TreeNodeInterface {
+    name: string;
+    caption? : string;
+    leftNode: TreeNodeInterface | null;
+    rightNode: TreeNodeInterface | null;
+}
+
+interface BinaryTreeProps {
+    rootNode: TreeNodeInterface | null;
+}
+
+function TreeNode() {
+    return (
+        <div>
+            Node
+        </div>);
+}
+
+const nodeMock: TreeNodeInterface = {
+    name: "Bartek",
+    caption: "is boss",
+    leftNode: null,
+    rightNode: null,
+}
+
+
+export function BinaryTree({rootNode} : BinaryTreeProps) {
+    const renderNode = (node: TreeNodeInterface | null) => {
+        if (!node) return null;
+
+        return (
+            <div className={"tree-wrapper"}>
+                <div className={"root-node"}>
+                    <p>{node.name}</p>
+                    <p>{node.caption}</p>
+                </div>
+                <div className={"left-child-node"}>{renderNode(node.leftNode)}</div>
+                <div className={"right-child-node"}>{renderNode(node.rightNode)}</div>
+            </div>
+        )
+    }
+    return(
+        <div>
+            {renderNode(rootNode)}
+        </div>
+    );
+}
+
+export default BinaryTree;

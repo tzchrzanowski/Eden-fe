ca67504f8384ff1f7a30954af7394ca7346becc6
tzchrzanowski <tzchrzanowski@gmail.com>
2023-10-23 13:46:14 +0800

    make network not visible for logged-out users

--- src/components/top-navigation/TopNavigation.tsx
+++ src/components/top-navigation/TopNavigation.tsx
@@ -1,33 +1,76 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './TopNavigation.css';
 import { Link } from "react-router-dom";
 import SideNavigation from "../side-navigation/SideNavigation";
-
+import UserContext from 'context/UserContext';
 
 interface buttonsInterface {
     route: string,
     caption: string
+    public: boolean
 };
 
 const buttons: buttonsInterface[] = [
-    {route: "/", caption: "Home"},
-    {route: "/ways-to-earn", caption: "Ways to earn"},
-    {route: "/products", caption: "Products"},
-    {route: "/contact", caption: "Contact"},
-    {route: "/network-chart", caption: "Network"},
-    {route: "/login", caption: "Login"},
+    {route: "/", caption: "Home", public: true},
+    {route: "/ways-to-earn", caption: "Ways to earn", public: true},
+    {route: "/products", caption: "Products", public: true},
+    {route: "/contact", caption: "Contact", public: true},
+    {route: "/network-chart", caption: "Network", public: false},
+    {route: "/login", caption: "Login", public: true},
 ]
 
 export function TopNavigation() {
     const [hoveredButtons, setHoveredButtons] = React.useState<boolean[]>(Array(buttons.length).fill(false));
     const [isSideNavOpen, setSideNavOpen] = React.useState(false);
 
+    const [roleUpdated, setRoleUpdated] = React.useState<boolean>(false);
+    const contextValue = useContext(UserContext);
+
+    /*
+    * Re-render top-navigation if role_id in context was updated
+    * */
+    React.useEffect(()=>{
+        console.log("re-render top navigation...", contextValue);
+    }, [roleUpdated]);
+
+    /*
+    * If user context contains role_id field, then set trigger for re-render
+    * */
+    React.useEffect(()=>{
+        if (contextValue &&
+            contextValue.state &&
+            contextValue.state.user &&
+            contextValue.state.user.role_id
+        ) {
+            setRoleUpdated(prevState => !prevState);
+        }
+    }, [contextValue]);
 
     const handleHover = (index: number, setter: boolean) => {
         const newHoveredButtons = [...hoveredButtons];
         newHoveredButtons[index] = setter;
         setHoveredButtons(newHoveredButtons);
     };
+
+    /*
+    * Display field if is public by default,
+    * If field is not public, should be displayed only if user_id type is 1 or 2
+    * */
+    const canDisplay = (isPublic: boolean): boolean => {
+        if (isPublic == false) {
+            if (contextValue &&
+                contextValue.state &&
+                contextValue.state.user &&
+                contextValue.state.user.role_id
+            ) {
+                const currentUserRoleId = contextValue.state.user.role_id;
+                return (currentUserRoleId === '1' || currentUserRoleId === '2') ?? true;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
 
     return (
         <>
@@ -37,15 +80,17 @@ export function TopNavigation() {
             />
             <div className={"nav-container"}>
                 {buttons && buttons.map((button, id) => {
-                    return <Link to={button.route} key={id}>
-                        <div
-                            className={(hoveredButtons[id] ? "isHovered button-box" : "button-box")}
-                            onMouseOver={() => handleHover(id, true)}
-                            onMouseLeave={() => handleHover(id, false)}
-                        >
-                            {button.caption}
-                        </div>
-                    </Link>
+                    if (canDisplay(button.public)) {
+                        return <Link to={button.route} key={id}>
+                            <div
+                                className={(hoveredButtons[id] ? "isHovered button-box" : "button-box")}
+                                onMouseOver={() => handleHover(id, true)}
+                                onMouseLeave={() => handleHover(id, false)}
+                            >
+                                {button.caption}
+                            </div>
+                        </Link>
+                    }
                 })}
                 <div
                     className={"button-box"}

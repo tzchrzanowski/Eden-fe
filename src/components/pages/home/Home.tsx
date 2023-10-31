285461450dc7668202a3f3c28856ffa2224ffa12
belle Llie <bellellie@belles-MacBook-Air.local>
2023-10-31 16:38:12 +0800

    added sliding images to home page

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -3,22 +3,42 @@ import './Home.css';
 import TopNavigation from "components/top-navigation/TopNavigation";
 import Video from "resources/videos/eden-blue-only.mp4";
 import VideoSpray from "resources/videos/eden-spray.mp4";
+import intro1 from "resources/images/slides/Intro_1.jpeg";
+import intro2 from "resources/images/slides/Intro_2.jpeg";
+import intro3 from "resources/images/slides/Intro_3.jpeg";
+
 export function Home() {
+    const [isVisible, setIsVisible]= React.useState< boolean>(true);
+
+    const handleSlideOut=()=>{
+        setIsVisible(false);
+    }
+
+    React.useEffect(()=> {
+        console.log("rerender...");
+    }, [isVisible])
     return (
         <div className={"HomeWrapper"}>
             <TopNavigation />
-            <div className={"homePageContent"}>
-                <video autoPlay loop muted height={"100%"} width={"100%"}>
+            {/* <div className={"homePageContent"}> */}
+                {/* <video autoPlay loop muted height={"100%"} width={"100%"}>
                     <source src={Video} type={"video/mp4"}/>
-                </video>
-            </div>
-            <div className={"homePageContent"}>
-                <video autoPlay loop muted height={"100%"} width={"100%"}>
+                </video> */}
+                {/* <video autoPlay loop muted height={"100%"} width={"100%"}>
                     <source src={VideoSpray} type={"video/mp4"}/>
-                </video>
-            </div>
-            <div className={"landing-page-caption"}>
-                <p>Eden Perfumes</p>
+                </video> */}
+            {/* </div> */}
+
+            <div className={"homePageContent"}>            
+                <img onClick={handleSlideOut} className={!isVisible ? 'image-slide out-of-screen' : "image-slide"} src={intro1} alt="img" />
+                {
+                    !isVisible && (
+                        <div className='img-container'>
+                            <img onClick={handleSlideOut} className={"image-box"} src={intro2} alt="img" />
+                            <img onClick={handleSlideOut} className={"image-box"} src={intro3} alt="img" />
+                        </div>
+                    )
+                }
             </div>
         </div>
     )

f597da520592638fff9f6a0c2c1f4cb4c01a649a
tzchrzanowski <tzchrzanowski@gmail.com>
2023-11-21 12:09:19 +0100

    added token validation to get user network request

--- src/data/getRequests.ts
+++ src/data/getRequests.ts
@@ -26,24 +26,28 @@ export async function getAllUsers() {
 
 export async function getUserNetwork(userId: number) {
     const apiUrl = endPointUrl + '/api/public/users/' +userId + '/get-network';
+    const token = localStorage.getItem("token");
 
-    try {
-        const response = await fetch(apiUrl, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            credentials: 'include',
-        });
+    if (typeof token == 'string') {
+        try {
+            const response = await fetch(apiUrl, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'AUTHORIZATION': token
+                },
+                credentials: 'include',
+            });
 
-        if (!response.ok) {
-            throw new Error(`Request failed with status: ${response.status}`);
+            if (!response.ok) {
+                throw new Error(`Request failed with status: ${response.status}`);
+            }
+
+            const users = await response.json();
+            return users;
+        } catch (error) {
+            console.error('Error fetching users:', error);
         }
-
-        const users = await response.json();
-        return users;
-    } catch (error) {
-        console.error('Error fetching users:', error);
     }
 }
 

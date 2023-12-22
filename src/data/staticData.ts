a401556aaf434af5bb63262d628113fa97168247
tzchrzanowski <tzchrzanowski@gmail.com>
2023-12-22 05:38:54 +0100

    changed production ip url

--- src/data/staticData.ts
+++ src/data/staticData.ts
@@ -1,3 +1,3 @@
 export const endPointUrl = "http://localhost:8080";
-export const endPointUrlProd = "http://13.212.71.251:8080";
+export const endPointUrlProd = "http://13.212.101.199";
 export const isProductionEnvironment = process.env.REACT_APP_ENVIRONMENT === 'production';
